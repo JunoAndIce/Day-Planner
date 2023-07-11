8704404256f1450e899db9833c346723e74352fb
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-11 15:23:18 -0500

    rewrote script.js with better formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,6 @@ function displayTime(){
 setInterval(displayTime, 1000);
 
 
-
 //loop through table and add button to the end of each row on the table
 let tr = document.querySelectorAll("table tbody tr");
 
@@ -26,7 +25,6 @@ Array.from(tr).forEach(function(trArray) {
 });
 
 
-
 $(".btn").click(function(event){
 
   var value =  $(this).closest("tr").find('.text').val();
@@ -39,34 +37,4 @@ $(".btn").click(function(event){
 
 })
 
-$('.time-block').each (function(index) {
-  //.attr('id').split('-')[1];
-  // get the specific ID of the time block and check with the current time 
-
-  var timeSection = $(this).attr('id').split('-')[1];
-  var currentTime = dayjs().format('H');
-  
-  console.log(timeSection + ' ' + currentTime);
-
-
-  if (timeSection === currentTime)
-  {
-    $(this).find('.text').addClass("present");
-  }
-  else if (timeSection > currentTime)
-  {
-    $(this).find('.text').addClass("future");
-  } 
-  else if (timeSection < currentTime)
-  {
-    $(this).find('.text').addClass("past");
-  } 
-  
-  // Local Storage Get
-  var savedEvent = localStorage.getItem(timeSection);
-
-  if (savedEvent) {
-    $(this).find('.text').val(savedEvent);
-  }
-});
 
