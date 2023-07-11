f1b60addc126736927c8ee4e0a2e74de780962eb
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-11 15:16:46 -0500

    Added colored box functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
+//DayJS Section
 var timeText = $('#1a');
-
 
 function displayTime(){
   var reformatDate = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
@@ -8,14 +8,82 @@ function displayTime(){
 
 setInterval(displayTime, 1000);
 
-//loop through table and add button to the table
+
+
+//loop through table and add button to the end of each row on the table
 let tr = document.querySelectorAll("table tbody tr");
 
 Array.from(tr).forEach(function(trArray) {
+
     let button = document.createElement("button");
     let td = document.createElement("td");
-    button.className = "fa-solid fa-save";
+
+    button.className = "fa-solid fa-save btn";
+    td.className = "btn_cont";
+
     td.append(button);
     trArray.append(td);
+});
+
+
+
+$(".btn").click(function(event){
+
+  var value =  $(this).closest("tr").find('.text').val();
+
+  var key = $(this).closest("tr").attr('id').split('-')[1];
+
+  console.log(key + ' ' + value);
+
+  localStorage.setItem(key, value);
+
+  console.log("Button pressed");
+})
+
+$('.time-block').each (function(index) {
+  //.attr('id').split('-')[1];
+  // get the specific ID of the time block and check with the current time 
+
+  var timeSection = $(this).attr('id').split('-')[1];
+  var currentTime = dayjs().format('H');
+  
+  console.log(timeSection + ' ' + currentTime);
+
+  if (timeSection === currentTime)
+  {
+    $(this).find('.text').addClass("present");
+  }
+  else if (timeSection > currentTime)
+  {
+    $(this).find('.text').addClass("future");
+
+  } 
+  else if (timeSection < currentTime)
+  {
+    $(this).find('.text').addClass("past");
+  } 
+  
+  var savedEvent = localStorage.getItem(timeSection);
+
+  if (savedEvent) {
+    $(this).find('.text').val(savedEvent);
+  }
+});
+
+function loadSettings() {
+  //Pull time key and value from local storage
+  // target specific text-area that has the same time as the row
+  // push event object into the textarea
+
+  // var currentTime = dayjs().hour;
+  // console.log(time);
+
+
+}
+
+// Saving to local storage and retrieving 
+$(document).ready(function() {
+  loadSettings();
+  
 });
 
