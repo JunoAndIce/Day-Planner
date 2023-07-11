d51fe307f7b860acf94e1a61ce658bc8557508b1
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-10 22:31:08 -0500

    Applied font awesome API and applied button to end of row

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,13 @@ function displayTime(){
 
 setInterval(displayTime, 1000);
 
-var table = document.getElementById('table');
-var btn = document.createElement("button");
-btn.innerHTML = "Save"; // Insert text
-
 //loop through table and add button to the table
 let tr = document.querySelectorAll("table tbody tr");
 
 Array.from(tr).forEach(function(trArray) {
     let button = document.createElement("button");
     let td = document.createElement("td");
-    button.innerText = "buy";
-    button.className = "btn_buy";
+    button.className = "fa-solid fa-save";
     td.append(button);
     trArray.append(td);
 });
